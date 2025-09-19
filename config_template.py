58442ff37231531dbbe5901c2c97427c6e7a074e
GFMws <ydalite01@gmail.com>
2025-09-19 10:00:59 +0400

    common template

---
+++ config_template.py
@@ -0,0 +1,19 @@
+# Config
+BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+
+# Database settings
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'USER_LOGIN',
+    'password': 'USER_PASSWORD',
+    'database': 'DATABASE_NAME',
+    'charset': 'utf8mb4'
+}
+
+# Proxy settings
+PROXY_CONFIG = {
+    'use_proxy': False,  # set False to disable proxy
+    'proxy_url': 'http://192.168.1.1:3333',
+    'username': '',  # leave it empty if not required
+    'password': ''   # leave it empty if not required
+}
